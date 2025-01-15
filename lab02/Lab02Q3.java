d6cfd17df0a4458080598725b80da50e7a09fe09
Deepak Maurya <dee221403@gmail.com>
2025-01-16 01:19:02 +0530

    Initial Commit

--- lab02/Lab02Q3.java
+++ lab02/Lab02Q3.java
@@ -5,11 +5,9 @@ public class Lab02Q3 {
     int[] arr = new int[args.length];
 
     for (int i=0; i<args.length; i++) arr[i] = Integer.parseInt(args[i]);
-
     Arrays.sort(arr, 0, arr.length);
 
     System.out.print("Sorted Array: ");
-
     for (int i=0; i<arr.length; i++) {
       System.out.print(arr[i]+ " ");
     }
